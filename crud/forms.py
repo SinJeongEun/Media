7ebd63a7e5535005fff74fe0ae2791a155e0713f
JeongEun <lisasje25@gmail.com>
2019-06-01 14:10:27 +0900

    plus media

---
+++ crud/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Blog,Comment,Hashtag
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['title','body','hashtags','image']
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields=['comment_text']
+
+class HashtagForm(forms.ModelForm):
+    class Meta:
+        model = Hashtag
+        fields = ['name']
+
+class MediaForm(forms.ModelForm):
+    model = Blog
+    fields = ['medias']
